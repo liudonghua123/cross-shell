61653c3ab7d86ecd6292f617642f36cd9b9c7177
liudonghua <liudonghua123@gmail.com>
2024-06-14 01:57:03 +0000

    only do parse_command on windows

--- bin/cross-shell.ts
+++ bin/cross-shell.ts
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 
 import { execute_command, parse_command } from '../lib/index.js'
+import type { Command } from '../lib/index.js'
+import debug from "debug";
+
+const log = debug("cross-shell:bin");
 
 (async () => {
     // check whether the first -n option is provided, which means dry run
@@ -9,7 +13,12 @@ import { execute_command, parse_command } from '../lib/index.js'
         process.argv.splice(2, 1);
     }
     const raw_command = process.argv.slice(2).join(' ');
-    const command = await parse_command(raw_command);
+    let command: Command = { command: raw_command, environments: [] };
+    // parse the command on windows
+    if (process.platform === 'win32') {
+        command = await parse_command(raw_command);
+        log(`Parsed command: ${JSON.stringify(command)}`);
+    }
     if (dry_run) {
         console.log(`Command: ${command.command}, Environments: ${command.environments.map((env) => `${env.name}=${env.value}`).join(' ')}`);
         process.exit(0);
